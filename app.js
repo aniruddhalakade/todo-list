a4a8ae1296e6aac5b3bee1d1639bb9adba0457e2
aniruddhalakade <119033276+aniruddhalakade@users.noreply.github.com>
2023-07-17 21:10:16 +0530

    Update app.js
    updated from to host problem faced

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = new mongoose.Schema({
   name: String
@@ -68,6 +69,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, async()=>{
-  await mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true}).then(console.log("Server started on port 3000"));
+app.listen(3000, ()=>{
+  console.log("Server started on port 3000");
 });
